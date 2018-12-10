11314c3c103b686bd6adaeab0222537c9a9f1e6f
Blythe Totsch <uncblythe1@gmail.com>
2018-12-10 14:22:23 -0600

    completed reversestring exercise w/3 solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+//-------------------------------------My work below:
+
+//input: string 
+//output: string( characters reversed )
+
+//1st solution
+// function reverse(str) {
+//     return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+//2nd solution
+function reverse(str) {
+    let reversed = '';
+    for(let i of str) {
+        reversed = i + reversed;
+    }
+    return reversed;
+}
+
+//3rd solution
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
+
 
 module.exports = reverse;
